d63206922c5153275d31f6a0e436c5e55e5433ad
Tom Hyde <thyde@neworbit.co.uk>
2024-09-11 11:39:08 +0100

    Do a conversation

--- Hackathon/Hackathon_Neworbit/Program.cs
+++ Hackathon/Hackathon_Neworbit/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using OpenAI.Chat;
 
 var builder = new HostApplicationBuilder();
 builder.Configuration.AddUserSecrets("c0515972-21aa-4f00-af01-2e17a542c4e1");
@@ -19,7 +20,23 @@ var kernel = Kernel.
     .Build();
 
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+Console.WriteLine("How can I help you today?");
+Console.Write("> ");
+var userInput = Console.ReadLine();
+
 var history = new ChatHistory();
-history.Add(new ChatMessageContent(AuthorRole.User, "How can I annihilate my boredom?"));
-var completions = await chatCompletionService.GetChatMessageContentsAsync(history);
-Console.WriteLine(completions.Last());
+
+history.AddSystemMessage("You will be a helpful AI. Your goal is to provide suggestions of what the user can do today to fulfil their entertainment requirements. You will respond in the style of The Terminator");
+history.AddUserMessage(userInput!);
+
+while (true)
+{
+    var completions = await chatCompletionService.GetChatMessageContentsAsync(history);
+    var assistantResponse = completions.Last();
+    Console.WriteLine(completions.Last());
+    Console.Write("> ");
+    var newUserInput = Console.ReadLine();
+    history.AddAssistantMessage(assistantResponse.InnerContent.ToString());
+    history.AddUserMessage(newUserInput!);
+}
