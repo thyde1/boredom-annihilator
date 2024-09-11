a71df9f3d5731cc656c873c8a9e18471d0e77cf6
Tom Hyde <thyde@neworbit.co.uk>
2024-09-11 10:40:26 +0100

    v1

--- Hackathon/Hackathon_Neworbit/Program.cs
+++ Hackathon/Hackathon_Neworbit/Program.cs
@@ -1,56 +1,16 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.SemanticKernel;
+﻿using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 
-var builder = Host.CreateEmptyApplicationBuilder(new HostApplicationBuilderSettings()
-{
-    Args = args
-} );
+Console.WriteLine("BOREDOM ANNIHIALATOR (v1)\n");
 
-// don't forget to add your api key / endpoint / deployment name/and model id ( deployments found here: https://oai.azure.com/ )
-builder.Services
-    //.AddAzureOpenAIAudioToText()
-    //.AddAzureOpenAITextEmbeddingGeneration()
-    //.AddAzureOpenAITextToImage()
-    .AddAzureOpenAIChatCompletion(
-        "",
-        "",
-        "",
-        modelId: "gpt-4o");
-        
-var app = builder.Build();
+Console.WriteLine("Boredom annihilation in progress...");
 
-var chat = app.Services.GetRequiredService<IChatCompletionService>();
+var kernel = Kernel.
+    CreateBuilder().AddAzureOpenAIChatCompletion("gpt4o", "https://boredom-euw-ai.openai.azure.com/", "5e24fb3095484dbfa47db36f61f9feb4")
+    .Build();
 
-var chatHistory = new ChatHistory();
-
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("AI: What am I?");
-Console.ForegroundColor = ConsoleColor.Yellow;
-
-Console.WriteLine("Jay: ");
-var whatAmI = Console.ReadLine();
-chatHistory.AddSystemMessage(whatAmI!);
-
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("AI: Cool! How can I help?");
-Console.ForegroundColor = ConsoleColor.Yellow;
-
-Console.WriteLine("Jay: ");
-var prompt = Console.ReadLine();
-chatHistory.AddUserMessage(prompt!);
-
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("AI: ");
-var response = await chat.GetChatMessageContentsAsync(chatHistory);
-var lastMessage = response.Last();
-Console.WriteLine(lastMessage);
-
-// You can create a kernal and pass it to `GetChatMessageContentsAsync`, you can add plugins to this kernal to control the behavior of the AI 
-// https://learn.microsoft.com/en-us/semantic-kernel/get-started/quick-start-guide?pivots=programming-language-csharp
-
-// detailed samples
-// https://learn.microsoft.com/en-us/semantic-kernel/get-started/detailed-samples?pivots=programming-language-csharp
-
-
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+var history = new ChatHistory();
+history.Add(new ChatMessageContent(AuthorRole.User, "How can I annihilate my boredom?"));
+var completions = await chatCompletionService.GetChatMessageContentsAsync(history);
+Console.WriteLine(completions.Last());
