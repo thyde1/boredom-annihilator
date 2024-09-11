d03c4395ff60d89be60d5eddc9408ca07fb696b0
Tom Hyde <thyde@neworbit.co.uk>
2024-09-11 11:59:02 +0100

    More tweaking

--- Hackathon/Hackathon_Neworbit/ChatPrinter.cs
+++ Hackathon/Hackathon_Neworbit/ChatPrinter.cs
@@ -1,5 +1,20 @@
 ﻿namespace Hackathon_Neworbit;
 
-public class ChatPrinter
+public static class ChatPrinter
 {
+    public static void PrintPrompt()
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("> ");
+        Console.ForegroundColor = ConsoleColor.Gray;
+    }
+
+    public static string GetUserInput()
+    {
+        PrintPrompt();
+        Console.ForegroundColor = ConsoleColor.Green;
+        var input =  Console.ReadLine()!;
+        Console.ForegroundColor = ConsoleColor.Gray;
+        return input;
+    }
 }
--- Hackathon/Hackathon_Neworbit/Program.cs
+++ Hackathon/Hackathon_Neworbit/Program.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Hackathon_Neworbit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
-using OpenAI.Chat;
 
 var builder = new HostApplicationBuilder();
 builder.Configuration.AddUserSecrets("c0515972-21aa-4f00-af01-2e17a542c4e1");
@@ -21,13 +21,18 @@ var kernel = Kernel.
 
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 
+Console.ForegroundColor = ConsoleColor.Gray;
+
 Console.WriteLine("How can I help you today?");
-Console.Write("> ");
-var userInput = Console.ReadLine();
+var userInput = ChatPrinter.GetUserInput();
 
 var history = new ChatHistory();
 
-history.AddSystemMessage("You will be a helpful AI. Your goal is to provide suggestions of what the user can do today to fulfil their entertainment requirements. You will respond in the style of The Terminator");
+history.AddSystemMessage(
+    @"You will be a helpful AI. Your goal is to provide suggestions of what the user can do today to fulfil their 
+    entertainment requirements. You will respond in the style of The Terminator. You should be weirdly obsessed with the weather.
+    You should check with the user whether you have successfully annihilated their boredom."
+);
 history.AddUserMessage(userInput!);
 
 while (true)
@@ -35,8 +40,7 @@ while (true)
     var completions = await chatCompletionService.GetChatMessageContentsAsync(history);
     var assistantResponse = completions.Last();
     Console.WriteLine(completions.Last());
-    Console.Write("> ");
-    var newUserInput = Console.ReadLine();
-    history.AddAssistantMessage(assistantResponse.InnerContent.ToString());
+    var newUserInput = ChatPrinter.GetUserInput();
+    history.AddAssistantMessage(assistantResponse.InnerContent!.ToString()!);
     history.AddUserMessage(newUserInput!);
 }
