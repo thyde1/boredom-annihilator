e986717c16b7395dc79b3b8a56e21851b1a49ce5
Tom Hyde <thyde@neworbit.co.uk>
2024-09-11 11:16:54 +0100

    Read key from user secrets

--- Hackathon/Hackathon_Neworbit/Program.cs
+++ Hackathon/Hackathon_Neworbit/Program.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.SemanticKernel;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+
+var builder = new HostApplicationBuilder();
+builder.Configuration.AddUserSecrets("c0515972-21aa-4f00-af01-2e17a542c4e1");
+var application = builder.Build();
 
 Console.WriteLine("BOREDOM ANNIHIALATOR (v1)\n");
 
 Console.WriteLine("Boredom annihilation in progress...");
 
+var configuration = application.Services.GetRequiredService<IConfiguration>();
+
 var kernel = Kernel.
-    CreateBuilder().AddAzureOpenAIChatCompletion("gpt4o", "https://boredom-euw-ai.openai.azure.com/", "5e24fb3095484dbfa47db36f61f9feb4")
+    CreateBuilder().AddAzureOpenAIChatCompletion("gpt4o", "https://boredom-euw-ai.openai.azure.com/", configuration.GetValue<string>("openaiKey")!)
     .Build();
 
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
